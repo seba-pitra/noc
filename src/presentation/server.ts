f878de4a6ab5cd76ae2e9eef5484382fd5e3cb2d
seba-pitra <sebastian-pitra10@gmail.com>
2024-04-30 18:49:46 -0300

    fix: add parameters in execution

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,13 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service"
 
 
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+    new FileSystemDatasource()
+)
 
 export class ServerApp {
     
@@ -12,9 +18,10 @@ export class ServerApp {
         const createdJob3 = CronService.createJob(
             '*/5 * * * * *',
             () => {
-                const url = 'http://localhost:3000'
+                const url = 'https://www.google.com';
 
                 new CheckService(
+                    fileSystemLogRepository,
                     () => console.log('success'),
                     (error) => console.error(error),
                 ).execute( url );
