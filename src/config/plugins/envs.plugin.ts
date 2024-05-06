171ea2cbb54a2e6d36a68bb98f669777d912f5d7
seba-pitra <sebastian.pitra10@gmail.com>
2024-05-05 22:53:27 -0300

    chore: connect mongo with docker

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -8,4 +8,8 @@ export const envs = {
     MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
     MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
     PROD: env.get('PROD').required().asBool(),
+    MONGO_URL: env.get('MONGO_URL').required().asString(),
+    MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
+    MONGO_DB_USER: env.get('MONGO_DB_USER').required().asString(),
+    MONGO_DB_PASS: env.get('MONGO_DB_PASS').required().asString(),
 }
