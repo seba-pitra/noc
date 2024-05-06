767e04ccbed6e85a4dbfa96c4b4e2ad1a55855ce
seba-pitra <sebastian.pitra10@gmail.com>
2024-05-05 23:40:22 -0300

    feat(infrastructure): create mongo datasource

---
+++ src/infrastructure/datasources/mongo-log.datasource.ts
@@ -0,0 +1,25 @@
+import { LogModel } from "../../data/mongo";
+import { LogDatasource } from "../../domain/datasources/log.datasource";
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
+
+
+
+export class MongoLogDatasource implements LogDatasource {
+
+    async saveLog( log: LogEntity ): Promise<void> {
+        const newLog = await LogModel.create( log );
+        await newLog.save();
+        console.log('Mongo Log Created', newLog.id);
+    }
+
+    async getLogs( severityLevel: LogSeverityLevel ): Promise<LogEntity[]> {
+        const foundLogs = await LogModel.find({
+            level: severityLevel
+        })
+
+        return foundLogs.map( mongoLog => LogEntity.fromObject(mongoLog) );
+    }
+
+}
+
+
